5efe42153a0a430aac47012e181222f80e1d83e3
IPCMF <95717050+IPCMF@users.noreply.github.com>
2024-08-24 21:40:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function calculate(){
     display.value = eval(display.value);
 }
 function infoPage(){
-    alert("This calculator app was made by IPCMF on GitHub.\nThis project was made possible thanks to 'Bro Code' on YouTube! [https://www.youtube.com/watch?v=I5kj-YsmWjM]\nThis Calculator was made using HTML, functions with Java and was given a glow-up by CSS.")
+    alert("This calculator app was made by IPCMF on GitHub.\nThis project was made possible thanks to 'Bro Code' on YouTube! [link on Public GitHub Page]\nThis Calculator was made using HTML, functions with JavaScript and was given a glow-up by CSS.")
 }
